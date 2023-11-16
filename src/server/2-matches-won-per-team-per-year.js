57fb551e9aa976fef6354302980f47862775ee49
Abhishek Sharma <sharmaabishek034@gmail.com>
2023-11-16 09:23:40 +0530

    gives the informations of matches won per year per team

---
+++ src/server/2-matches-won-per-team-per-year.js
@@ -0,0 +1,36 @@
+//Number of matches won per team per year in IPL.
+
+const readCSVFile = require('./convert.js');
+
+const csvFilePath = '/home/abhishek/ipl/src/data/matches.csv';
+
+readCSVFile(csvFilePath).then((matchesData) => {
+  var yearwise = WonPerYear(matchesData);
+  console.log(JSON.stringify(yearwise));
+});
+
+function WonPerYear(matchesData) {
+  let WonPerYear = matchesData.reduce((PerYear, match) => {
+    if (!PerYear[match.season]) {
+      PerYear[match.season] = years(matchesData, match.season);
+    }
+    return PerYear;
+  }, {});
+  return WonPerYear;
+}
+
+function years(matchesData, year) {
+  var conn = {};
+  let data = matchesData.reduce((MatchWon, match) => {
+    if (match.season == year && match.winner) {
+      if (!MatchWon[match.winner]) {
+        MatchWon[match.winner] = 1;
+      } else {
+        MatchWon[match.winner]++;
+      }
+    }
+
+    return MatchWon;
+  }, {});
+  return data;
+}

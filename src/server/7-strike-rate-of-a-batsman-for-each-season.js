1710537a02d582b15467e474fdcf9931c6fe5ef3
Abhishek Sharma <sharmaabishek034@gmail.com>
2023-11-18 18:30:21 +0530

    changed the functionality to get strickrate of a perticular by specfing and if not specifyed the data of all

--- src/server/7-strike-rate-of-a-batsman-for-each-season.js
+++ src/server/7-strike-rate-of-a-batsman-for-each-season.js
@@ -1,17 +1,17 @@
-//Find the strike rate of a batsman for each season
 function strikeRateOfaBatsmanForEachSeason(
   matchesData,
   deliveriesdata,
   player = ''
 ) {
   var strick = {};
-
   deliveriesdata.reduce((info, ball) => {
     var year = matchesData[ball.match_id - 1].season;
+
     if (!info[ball.batsman]) {
       info[ball.batsman] = { [year]: { run: 0, ball: 0 } };
       strick[ball.batsman] = { [year]: 0 };
     }
+
     if (!info[ball.batsman][year]) {
       info[ball.batsman][year] = { run: 0, ball: 0 };
       strick[ball.batsman][year] = 0;
@@ -20,6 +20,7 @@ function strikeRateOfaBatsmanForEachSeason(
     info[ball.batsman][year].run += Number.parseInt(
       ball.batsman_runs
     );
+
     if (ball.wide_runs == 0) {
       info[ball.batsman][year].ball += 1;
     }

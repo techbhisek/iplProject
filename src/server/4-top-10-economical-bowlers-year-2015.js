1eac23bad775c3e00002a7a09b9904f8b63e0d28
Abhishek Sharma <sharmaabishek034@gmail.com>
2023-11-18 18:25:43 +0530

    added sorting to get top 10

--- src/server/4-top-10-economical-bowlers-year-2015.js
+++ src/server/4-top-10-economical-bowlers-year-2015.js
@@ -1,39 +1,40 @@
-//Top 10 economical bowlers in the year 2015
 function top10EconomicalBowlersYear2015(matchesData, deliveries) {
   var stats = {};
   deliveries.reduce((EconomyData, ball) => {
     if (matchesData[ball.match_id - 1].season == 2015) {
-      if (!stats[ball.bowler]) {
-        stats[ball.bowler] = 0;
-      }
+      /*First finding the match id of a delivery , using this same match id the season in the matchData as assuming 
+    it as array and starting index is 0 . so match==match_id -1 if it is 2016*/
       if (!EconomyData[ball.bowler]) {
-        EconomyData[ball.bowler] = { run: 0, ball: 0 };
+        EconomyData[ball.bowler] = { run: 0, ball: 0 }; // checking for the bowler in the object contains runs and bowles
+        stats[ball.bowler] = 0; // parlally creating one for object for there economy
       }
       EconomyData[ball.bowler].run +=
-        Number.parseInt(ball.wide_runs) +
-        Number.parseInt(ball.noball_runs) +
-        Number.parseInt(ball.batsman_runs);
+        parseInt(ball.wide_runs) +
+        parseInt(ball.noball_runs) +
+        parseInt(ball.batsman_runs); // adding the runs to bowler runs account
 
       if (ball.noball_runs == 0 && ball.wide_runs == 0) {
+        //only ball is counted if it is not noball and not a wide ball
         EconomyData[ball.bowler].ball += 1;
       }
       stats[ball.bowler] = (
         EconomyData[ball.bowler].run /
         (EconomyData[ball.bowler].ball / 6)
-      ).toFixed(2);
+      ).toFixed(2); //updating the economy of the bowler after getting there runs every ball;
     }
-
-    return EconomyData;
+    return EconomyData; // returning to get updated  runs and balls after everyball
   }, {});
 
-  let sort = [];
+  let sorted = [];
   Object.entries(stats).map((economy) => {
-    sort.push([economy[0], economy[1]]);
+    // pushing the Bowler and it economy to arrays as not possibel to sort object directly
+    sorted.push([economy[0], economy[1]]);
   });
 
-  sort.sort((player1, player2) => player1[1] - player2[1]);
+  sorted.sort((player1, player2) => player1[1] - player2[1]); // sort the array
 
-  return sort.slice(0, 10).reduce((sorted, player) => {
+  return sorted.slice(0, 10).reduce((sorted, player) => {
+    // converts the array into objects and then return
     sorted.push({ [player[0]]: player[1] });
     return sorted;
   }, []);

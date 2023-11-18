99d6596b3d728143ed08c9009c5e54172f64ec08
Abhishek Sharma <sharmaabishek034@gmail.com>
2023-11-18 18:26:52 +0530

    added comments and optimized

--- src/server/5-number-of-times-each-team-won-toss-and-match.js
+++ src/server/5-number-of-times-each-team-won-toss-and-match.js
@@ -1,13 +1,14 @@
-//Find the number of times each team won the toss and also won the match
 function numberOfTimesEachTeamWonTossAndMatch(matchesData) {
   return matchesData.reduce((conn, match) => {
     if (match.winner == match.toss_winner) {
+      //checks if the toss winner isEquals to match Winner
       if (!conn[match.winner]) {
+        //if team is availabe or not .if not then creats one object and assign 0
         conn[match.winner] = 0;
       }
-      conn[match.winner]++;
+      conn[match.winner]++; // increases the value. if the team wins it wins
     }
-    return conn;
+    return conn; // return to check each time if available or not helps in updating after every match
   }, {});
 }
 

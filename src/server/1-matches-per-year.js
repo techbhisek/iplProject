8dabedd34532c8402ffc24df2f21ae74e7d59195
Abhishek Sharma <sharmaabishek034@gmail.com>
2023-11-18 18:19:10 +0530

    gives the data of Matches per year

--- src/server/1-matches-per-year.js
+++ src/server/1-matches-per-year.js
@@ -1,12 +1,14 @@
 function matchesPerYear(matchesData) {
   return matchesData.reduce((TeamMatchs, match) => {
-    if (!TeamMatchs[match.season]) {
-      TeamMatchs[match.season] = 0;
+    //reduce helps in iterating through the MatchesData and give information of each match in match object
+    if (match.season) {
+      //it checks wheather season is defined in the match if not then return false and skip that match data
+      if (!TeamMatchs[match.season]) {
+        TeamMatchs[match.season] = 0; //if the is no season object is present then create and initialize it with 0
+      }
+      TeamMatchs[match.season]++; //incermenting the matches in that season of ipl
     }
-    TeamMatchs[match.season]++;
-
-    return TeamMatchs;
+    return TeamMatchs; //returning the data by adding the season .so that we hold data of every season with out loss
   }, {});
 }
-
 module.exports = matchesPerYear;

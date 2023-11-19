60e2ad5e75392bc90b9aa296ca7c2218e75b6d8d
Abhishek Sharma <sharmaabishek034@gmail.com>
2023-11-19 22:26:25 +0530

    Done the requid modifications

--- src/server/convert.js
+++ src/server/convert.js
@@ -1,23 +1,34 @@
+const csvtojson = require('csvtojson');
 const fs = require('fs');
-const csv = require('csv-parser');
 
-function readCSVFile(csvFilePath) {
-  return new Promise((resolve, reject) => {
-    const csvDataArray = [];
+const csvFilePath1 = './src/data/deliveries.csv';
+const csvFilePath2 = './src/data/matches.csv';
 
-    const stream = fs
-      .createReadStream(csvFilePath)
-      .pipe(csv())
-      .on('data', (match) => {
-        csvDataArray.push(match);
-      })
-      .on('end', () => {
-        resolve(csvDataArray);
-      })
-      .on('error', (error) => {
-        reject(error);
+const jsonFilePath1 = './src/data/deliveriesInJson.json';
+const jsonFilePath2 = './src/data/matchesInJson.json';
+
+// Function to convert CSV to JSON and write to file
+function convertCsvToJson(csvFilePath, jsonFilePath) {
+  csvtojson()
+    .fromFile(csvFilePath)
+    .then((jsonArrayObj) => {
+      const jsonString = JSON.stringify(jsonArrayObj, null, 2);
+
+      fs.writeFile(jsonFilePath, jsonString, 'utf8', (err) => {
+        if (err) {
+          console.error('Error writing JSON file:', err);
+        } else {
+          console.log(
+            'JSON file has been written successfully:',
+            jsonFilePath
+          );
+        }
       });
-  });
+    })
+    .catch((err) => {
+      console.error('Error converting CSV to JSON:', err);
+    });
 }
 
-module.exports = readCSVFile;
+convertCsvToJson(csvFilePath1, jsonFilePath1);
+convertCsvToJson(csvFilePath2, jsonFilePath2);

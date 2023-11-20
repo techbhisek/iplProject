5826634a36480019bee0748416f51ddd593e4cff
Abhishek Sharma <sharmaabishek034@gmail.com>
2023-11-20 11:52:54 +0530

    requied changes are made

--- src/server/3-extra-runs-conceded-per-team-year-2016.js
+++ src/server/3-extra-runs-conceded-per-team-year-2016.js
@@ -4,16 +4,21 @@ function extraRunsConcededPerTeamYear2016(
 ) {
   return deliveriesdata.reduce((ExtraPerTeam, ball) => {
     //using the reduce iterating through each and every delivery
-    if (matchesData[ball.match_id - 1].season == 2016) {
-      /*First finding the match id of a delivery , using this same match id the season in the matchData as assuming 
+    if (matchesData != []) {
+      //console.log();
+      if (
+        matchesData[ball.match_id - matchesData[0].id].season == 2016
+      ) {
+        /*First finding the match id of a delivery , using this same match id the season in the matchData as assuming 
       it as array and starting index is 0 . so match==match_id -1 if it is 2016*/
-      if (!ExtraPerTeam[ball.bowling_team]) {
-        //checking bowling team is present or not if not create one
-        ExtraPerTeam[ball.bowling_team] = 0;
+        if (!ExtraPerTeam[ball.bowling_team]) {
+          //checking bowling team is present or not if not create one
+          ExtraPerTeam[ball.bowling_team] = 0;
+        }
+        ExtraPerTeam[ball.bowling_team] += Number.parseInt(
+          ball.extra_runs
+        ); //Adding extras to there runs
       }
-      ExtraPerTeam[ball.bowling_team] += Number.parseInt(
-        ball.extra_runs
-      ); //Adding extras to there runs
     }
     return ExtraPerTeam;
   }, {});

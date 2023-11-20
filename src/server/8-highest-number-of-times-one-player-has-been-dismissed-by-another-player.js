44a293c2590e149dc29926e43125ba29fb758f12
Abhishek Sharma <sharmaabishek034@gmail.com>
2023-11-20 14:55:47 +0530

    added the comments

--- src/server/8-highest-number-of-times-one-player-has-been-dismissed-by-another-player.js
+++ src/server/8-highest-number-of-times-one-player-has-been-dismissed-by-another-player.js
@@ -11,20 +11,22 @@ function highestNumberOfTimesOnePlayerHasBeenDismissedByAnotherPlayer(
       ball.dismissal_kind != 'run out' &&
       ball.dismissal != 'retired hurt'
     ) {
+      //checks if the dissimissal is not by run out and retired hurt
       if (!bowler[ball.bowler]) {
-        bowler[ball.bowler] = { [ball.player_dismissed]: 0 };
+        bowler[ball.bowler] = { [ball.player_dismissed]: 0 }; // creates for a bowler if the object is not present
       }
       if (!bowler[ball.bowler][ball.player_dismissed]) {
-        bowler[ball.bowler][ball.player_dismissed] = 0;
+        bowler[ball.bowler][ball.player_dismissed] = 0; // checks for a particular is the dismissals is already present if not creates one
       }
       bowler[ball.bowler][ball.player_dismissed]++;
       if (bowler[ball.bowler][ball.player_dismissed] >= times) {
+        //this is to check the current max
         player = ball.bowler;
         times = bowler[ball.bowler][ball.player_dismissed];
         dismissed = ball.player_dismissed;
 
         if (!wickets[times]) {
-          wickets[times] = { [player]: dismissed };
+          wickets[times] = { [player]: dismissed }; //add parallel in the wickets of highest
         }
         if (!wickets[times][player]) {
           wickets[times][player] = dismissed;
@@ -36,7 +38,7 @@ function highestNumberOfTimesOnePlayerHasBeenDismissedByAnotherPlayer(
     return bowler;
   }, {});
 
-  return { [times]: wickets[times] };
+  return { [times]: wickets[times] }; // as we have the maximum number of times we diectly return it.
 }
 
 module.exports =

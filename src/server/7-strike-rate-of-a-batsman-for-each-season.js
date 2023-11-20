88bd639fea32e9492d920d332335b5e96d52b0bb
Abhishek Sharma <sharmaabishek034@gmail.com>
2023-11-20 14:56:08 +0530

    made the require changes

--- src/server/7-strike-rate-of-a-batsman-for-each-season.js
+++ src/server/7-strike-rate-of-a-batsman-for-each-season.js
@@ -1,40 +1,46 @@
 function strikeRateOfaBatsmanForEachSeason(
   matchesData,
-  deliveriesdata,
+  deliveriesData,
   player = ''
 ) {
   var strick = {};
-  deliveriesdata.reduce((info, ball) => {
-    var year = matchesData[ball.match_id - 1].season;
+  deliveriesData.reduce((info, ball) => {
+    if (matchesData != []) {
+      var year =
+        matchesData[ball.match_id - matchesData[0].id].season; //this single line can find  season of the match without using any map or filter .
+      //first finding the index of that delivery in match then finding it season
 
-    if (!info[ball.batsman]) {
-      info[ball.batsman] = { [year]: { run: 0, ball: 0 } };
-      strick[ball.batsman] = { [year]: 0 };
+      if (!info[ball.batsman]) {
+        //checks for the batsman in the object. if not present create one
+        info[ball.batsman] = { [year]: { run: 0, ball: 0 } };
+        strick[ball.batsman] = { [year]: 0 }; //parallel creating the strike rate
+      }
+
+      if (!info[ball.batsman][year]) {
+        info[ball.batsman][year] = { run: 0, ball: 0 }; //check for a perticular player an year is present
+        strick[ball.batsman][year] = 0;
+      }
+
+      info[ball.batsman][year].run += Number.parseInt(
+        ball.batsman_runs
+      );
+
+      if (ball.wide_runs == 0) {
+        info[ball.batsman][year].ball += 1;
+      }
+      strick[ball.batsman][year] = Math.ceil(
+        (info[ball.batsman][year].run /
+          info[ball.batsman][year].ball) *
+          100
+      );
     }
-
-    if (!info[ball.batsman][year]) {
-      info[ball.batsman][year] = { run: 0, ball: 0 };
-      strick[ball.batsman][year] = 0;
-    }
-
-    info[ball.batsman][year].run += Number.parseInt(
-      ball.batsman_runs
-    );
-
-    if (ball.wide_runs == 0) {
-      info[ball.batsman][year].ball += 1;
-    }
-    strick[ball.batsman][year] = Math.ceil(
-      (info[ball.batsman][year].run / info[ball.batsman][year].ball) *
-        100
-    );
     return info;
   }, {});
 
   if (player != '') {
-    return { [player]: strick[player] };
+    return { [player]: strick[player] }; // if player is defined then gives data of him
   }
-  return strick;
+  return strick; // if player is not defined then
 }
 
 module.exports = strikeRateOfaBatsmanForEachSeason;
